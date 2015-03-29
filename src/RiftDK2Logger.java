defbca7389a6d10431e7bd7014d50370630751fc
Chris Barr <chris@192-168-1-5.tpgi.com.au>
2015-03-29 12:12:26 +1030

    Logging More Data
    Now logging angular velocity and acceleration.

--- src/RiftDK2Logger.java
+++ src/RiftDK2Logger.java
@@ -19,8 +19,9 @@ import com.oculusvr.capi.TrackingState;
 
 public class RiftDK2Logger {
 	
-	private static SensorData latestData = new SensorData(0, 0, 0, 0, 0, 0, 0, 0);
+	private static SensorData latestData = new SensorData(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 	private static boolean run = true;
+	private static final long TIME_BETWEEN_SAMPLES = 25;
 	
 	public static void main(String[] args) throws UnknownHostException {
 		 
@@ -71,6 +72,7 @@ public class RiftDK2Logger {
 		 
 	        private final Hmd hmd;
 	        private long startTime = System.currentTimeMillis();
+	        private long lastReadingTime = startTime;
 	 
 	        public SensorFetcher(Hmd hmd) {
 	            this.hmd = hmd;
@@ -94,10 +96,14 @@ public class RiftDK2Logger {
 	        	
 	            while (run) {
 	                TrackingState sensorState = hmd.getSensorState(Hmd.getTimeInSeconds());
+	                
+	               
 	 
 	                OvrVector3f pos = sensorState.HeadPose.Pose.Position;
 	                OvrQuaternionf quat = sensorState.HeadPose.Pose.Orientation;
-	 
+	                OvrVector3f accel = sensorState.HeadPose.AngularAcceleration;
+	                OvrVector3f veloc = sensorState.HeadPose.AngularVelocity;
+	                
 	                double px = pos.x;
 	                double py = pos.y;
 	                double pz = pos.z;
@@ -106,16 +112,32 @@ public class RiftDK2Logger {
 	                double qy = quat.y;
 	                double qz = quat.z;
 	                double qw = quat.w;
+	                
+	                double ax = accel.x;
+	                double ay = accel.y;
+	                double az = accel.z;
+	                
+	                double vx = veloc.x;
+	                double vy = veloc.y;
+	                double vz = veloc.z;
 	 
-	                latestData = new SensorData(System.currentTimeMillis(), px, py, pz, qx, qy, qz, qw);
+	                latestData = new SensorData(System.currentTimeMillis(), px, py, pz, qx, qy, qz, qw, ax, ay, az, vx, vy, vz);
 	                System.out.println(latestData.toString());
+	                
 	                if(printWriter != null)
 	                {
 	                	long timeSinceStart = System.currentTimeMillis() - startTime;
 	                	printWriter.println(timeSinceStart + "\t" + latestData.toString());
 	                }
+	                
 	                try {
-	                    Thread.sleep(1);
+	                	while(System.currentTimeMillis() - lastReadingTime < TIME_BETWEEN_SAMPLES) 
+	                	{
+	                		Thread.sleep(1);
+	                	}
+	                	
+	                	lastReadingTime = System.currentTimeMillis();
+	                	
 	                } catch (InterruptedException ex) {
 	                    //
 	                }
@@ -131,9 +153,10 @@ public class RiftDK2Logger {
 	private static class SensorData {
 		 
         private final long id;
-        private final double px, py, pz, qx, qy, qz, qw;
+        private final double px, py, pz, qx, qy, qz, qw, ax, ay, az, vx, vy, vz;
+        private double pitch, roll, yaw;
  
-        public SensorData(long id, double px, double py, double pz, double qx, double qy, double qz, double qw) {
+        public SensorData(long id, double px, double py, double pz, double qx, double qy, double qz, double qw, double ax, double ay, double az, double vx, double vy, double vz) {
             this.id = id;
             this.px = px;
             this.py = py;
@@ -142,6 +165,17 @@ public class RiftDK2Logger {
             this.qy = qy;
             this.qz = qz;
             this.qw = qw;
+            
+            this.ax = ax;
+            this.ay = ay;
+            this.az = az;
+            
+            this.vx = vx;
+            this.vy = vy;
+            this.vz = vz;
+            
+            
+            
         }
  
         public long getId() {
@@ -154,7 +188,7 @@ public class RiftDK2Logger {
  
         @Override
         public String toString() {
-            return String.format("%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f", px, py, pz, qx, qy, qz, qw);
+            return String.format("%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f", px, py, pz, qx, qy, qz, qw,ax,ay,az,vx,vy,vz);
         	//return String.format("Position: %.3f  %.3f  %.3f | Quat:  %.3f  %.3f  %.3f  %.3f", px, py, pz, qx, qy, qz, qw);
         }
     }
